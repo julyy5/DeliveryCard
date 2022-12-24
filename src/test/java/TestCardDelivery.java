074afe7be61a92a2b31eb6426461a9a6ef679c52
julyy5 <smirnova-1999@inbox.ru>
2022-12-24 17:10:45 +0300

    Initial commit

--- src/test/java/TestCardDelivery.java
+++ src/test/java/TestCardDelivery.java
@@ -1,5 +1,6 @@
-import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
 import java.time.Duration;
 import java.time.LocalDate;
@@ -9,22 +10,28 @@ import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+
 public class TestCardDelivery {
+
+    public String generateDate(long addDays, String pattern) {
+        return LocalDate.now().plusDays(5).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    private final String Date = generateDate(5, "dd.MM.yyyy");
+
     @Test
-    void test() {
-        Configuration.headless = true;
-        LocalDate date = LocalDate.now().plusDays(3);
-        String dateText = date.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    void testFormOfDeliveryCard() {
         open("http://localhost:9999");
         $x("//input[@placeholder=\"Город\"]").setValue("Санкт-Петербург");
-        $x("//input[@placeholder=\"Дата встречи\"]").setValue(dateText);
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $x("//input[@placeholder=\"Дата встречи\"]").setValue(Date);
         $("[name=\"name\"]").setValue("Анна-Мария Петрова-Сидорова");
         $("[name=\"phone\"]").setValue("+71234567890");
         $("[data-test-id=\"agreement\"]").click();
         $(".button").click();
-        $("[data-test-id=\"notification\"]").should(visible, Duration.ofSeconds(15));
-        String text = $("[class=\"notification__content\"]").getText();
-        assertEquals("Встреча успешно забронирована на " + dateText, text);
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + Date), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
     }
 
 }
